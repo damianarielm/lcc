b16a4332d3fa48066c6217040b0812d20d238f9a
damianarielm <drd00m@msn.com>
2020-04-20 03:31:16 -0300

    Implemento problema de las 4 esquinas en TP1 de IIA.

--- 4 Ano/Introduccion a la Inteligencia Artificial/Soluciones/TP1/searchAgents.py
+++ 4 Ano/Introduccion a la Inteligencia Artificial/Soluciones/TP1/searchAgents.py
@@ -281,12 +281,12 @@ class CornersProblem(search.SearchProblem):
     def getStartState(self):
         "Returns the start state (in your state space, not the full Pacman state space)"
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return (self.startingPosition, list(self.corners))
 
     def isGoalState(self, state):
         "Returns whether this search state is a goal state of the problem"
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return len(state[1]) == 0
 
     def getSuccessors(self, state):
         """
@@ -310,6 +310,14 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x, y = state[0]
+            dx, dy = Actions.directionToVector(action)
+            newPos = nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+
+            if not hitsWall:
+                newCorners = [ c for c in state[1] if c != newPos ]
+                successors += [((newPos, newCorners), action, 1)]
 
         self._expanded += 1
         return successors
